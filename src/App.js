6e044f3650d3156e525fc64731ae1b483c19bb6b
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-19 10:50:25 -0400

    Adding new color to the restart button and text on the screen indicating the winner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import {useState} from 'react'
 
 function App() {
+  const winnerText = document.getElementById("winnerText");
   const cells = document.querySelectorAll("td");
   const [letter, setletter] = useState("X");
 
@@ -38,41 +39,49 @@ function App() {
       alert(cells[0].innerText + " is the winner");
       setletter("X");
       highlight(0, 1, 2);
+      winnerText.textContent = `The winner of the last game is: ${cells[0].innerText}`;
 
     }else if(cells[0].innerText !=="" && cells[0].innerText === cells[4].innerText && cells[4].innerText === cells[8].innerText){
       alert(cells[0].innerText + " is the winner");
       highlight(0, 4, 8);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[0].innerText}`;
 
     }else if(cells[3].innerText !=="" && cells[3].innerText === cells[4].innerText && cells[4].innerText === cells[5].innerText){
       alert(cells[3].innerText + " is the winner");
       highlight(3, 4, 5);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[3].innerText}`;
 
     }else if(cells[2].innerText !=="" && cells[2].innerText === cells[4].innerText && cells[4].innerText === cells[6].innerText){
       alert(cells[2].innerText + " is the winner");
       highlight(2, 4, 6);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[2].innerText}`;
 
     }else if(cells[6].innerText !=="" && cells[6].innerText === cells[7].innerText && cells[7].innerText === cells[8].innerText){
       alert(cells[6].innerText + " is the winner");
       highlight(6, 7, 8);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[6].innerText}`;
 
     }else if(cells[0].innerText !=="" && cells[0].innerText === cells[3].innerText && cells[3].innerText === cells[6].innerText){
       alert(cells[0].innerText + " is the winner");
       highlight(0, 3, 6);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[0].innerText}`;
 
     }else if(cells[1].innerText !=="" && cells[1].innerText === cells[4].innerText && cells[4].innerText === cells[7].innerText){
       alert(cells[1].innerText + " is the winner");
       highlight(1, 4, 7);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[1].innerText}`;
 
     }else if(cells[2].innerText !=="" && cells[2].innerText === cells[5].innerText && cells[5].innerText === cells[8].innerText){
       alert(cells[2].innerText + " is the winner");
       highlight(2, 5, 8);
       setletter("X");
+      winnerText.textContent = `The winner of the last game is: ${cells[2].innerText}`;
     }
   }
 
@@ -84,7 +93,7 @@ function App() {
   return (
     <> 
     <div className='holder'>
-      <h1 className='header'>Tic Tac Toe #️⃣</h1>
+      <h1 className='header'>Tic-Tac-Toe #️⃣</h1>
       <table>
         <tbody>
           <tr>
@@ -105,7 +114,8 @@ function App() {
         </tbody>
       </table>
 
-      <button onClick={resetBoard}>Restart</button>
+      <button className='restart-btn' onClick={resetBoard}>Restart</button>
+      <h4 id='winnerText'></h4>
       </div>
     </>
   );
